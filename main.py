8acb2db4d6d64406b3971e40e1ed9511be4e3480
J-Wengler <jwengler@gmail.com>
2023-02-24 10:32:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from yeelight import Bulb
 new_bulb = Bulb("192.168.1.64")
 new_bulb.turn_off()
 quit(1)
-thunder = ThunderHead("Mandajacobson98", "Cayman2ajw!!", change = -30, low = 60, high = 275)
+thunder = ThunderHead("DEXCOMUSER", "DEXCOMPASS", change = -30, low = 60, high = 275)
 try:
     thunder.watch_efficient()
 except Exception as e:
@@ -14,8 +14,8 @@ except Exception as e:
     conn = http.client.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
     urllib.parse.urlencode({
-        "token": "a7hxahk94ajn8tyt5y13wnq4dwia1t",
-        "user": "u39yretpf7kpu775jm8djwey42t8jh",
+        "token": "PUSHOVERTOKEN",
+        "user": "PUSHOVERUSER",
         "title": "ThunderHead Issue",
         "message": f"TEST SERVER FAILED WITH ERROR: {e}",
     }), { "Content-type": "application/x-www-form-urlencoded" })
